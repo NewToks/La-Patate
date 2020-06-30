2af64e0657be0c7170f2151b95adaf8b2daa1503
NewToks <61874752+NewToks@users.noreply.github.com>
2020-06-30 18:44:50 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,10 +4,10 @@ module.exports.run = async (client, message) =>{
 
     var help_embed = new Discord.MessageEmbed()
         .setColor("7289DA")
-        /*.setThumbnail("#")*/
         .setTitle("**__LISTE DE COMMANDES__**")
-        .setDescription("Le préfixe **.** est à mettre avant chaque commande")
-        .addField("rien pour l'instant")
+        .setDescription("Le préfixe **/** est à mettre avant chaque commande")
+        .addField("help" , "Affiche cette commande")
+        .addField("clear <nombre>" , "Supprime le nombre de message demandé")
         .setTimestamp()
         .setFooter("Created By NewToks#8330")
         message.channel.send(help_embed)
